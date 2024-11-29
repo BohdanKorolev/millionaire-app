e7557bc2ca1e253930248b3b02f99005f283845f
BohdanKorolev <bohdan.korolev@gmail.com>
2024-11-29 10:00:24 +0200

    feat: display the result on final page

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,3 +1,23 @@
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+
 export default function Result() {
-  return <div>this is result page</div>;
+  const searchParams = useSearchParams();
+  const score = searchParams.get('score') || '0';
+
+  return (
+    <div>
+      <h1>Game Over</h1>
+      <p>Your final score is: {score}</p>
+      <button
+        type="button"
+        onClick={() => {
+          window.location.href = '/';
+        }}
+      >
+        Play Again
+      </button>
+    </div>
+  );
 }
